a936b0560db29332f7159487cf94f0f2204ae2f9
Javipaur <javipaur@gmail.com>
2022-07-29 18:17:39 +0200

    tag

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,5 @@
 import {Router} from'express';
 import {indexWelcome} from '../controllers/index.controller';
 const router =Router();
-
     router.route('/').get(indexWelcome);
-  
-
 export default router;
--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -2,7 +2,5 @@ import {Router} from 'express';
 import {getUsuarios} from '../controllers/usuarios.controller';
 
 const router=Router();
-
 router.route('/').get(getUsuarios); 
-
 export default router;
