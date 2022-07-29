795b635e8882e196115123df33664c508aaa4d9a
Javipaur <javipaur@gmail.com>
2022-07-29 18:02:37 +0200

    config

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -3,6 +3,6 @@ import {getUsuarios} from '../controllers/usuarios.controller';
 
 const router=Router();
 
-router.route('/').get(getUsuarios);
+router.route('/').get(getUsuarios); 
 
 export default router;
